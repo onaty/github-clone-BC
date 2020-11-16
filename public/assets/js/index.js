1890cd58601c0cf0bee3679e88aaaa8872fbb86a
Oluwatobi Onasanya <tobi@wallet.ng>
2020-11-16 01:26:09 +0100

    Nav bar for mobile and web

---
+++ public/assets/js/index.js
@@ -0,0 +1,9 @@
+function toggleburgmenu(value) {
+    var x = document.getElementById("mobilelistdropdown");
+    
+    if (x.style.display != "block") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
